2e18c6ec0e40210f256618d9a3a5d673fb0f5ba0
AverinDS <awerin960@yandex.ru>
2017-10-02 22:00:08 +0400

    Set adminka

--- server/page1/admin.py
+++ server/page1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import shop, product
+
+admin.site.register(shop)
+admin.site.register(product)
 
 # Register your models here.
--- server/page1/models.py
+++ server/page1/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class shop(models.Model):
+    name = models.TextField(default="Empty")
+    adress = models.TextField(default="Empty")
+
+    def __str__(self):
+        return self.name +" "+ self.adress
+
+    class Meta:
+        db_table = "shop"
+
+class product(models.Model):
+    name = models.TextField(default="Empty")
+    price = models.FloatField(default=0.0)
+    shopFK = models.ForeignKey(shop)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "product"
+
