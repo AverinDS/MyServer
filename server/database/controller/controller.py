68ae97d5c748e4e175a2d4c1631275e31457e81a
AverinDS <awerin960@yandex.ru>
2017-10-15 13:47:01 +0400

    Added POST. Not tested!

--- server/database/controller/controller.py
+++ server/database/controller/controller.py
@@ -1,50 +1,53 @@
 from django.http import HttpResponse, JsonResponse
-from ..models.Shop import Shop
-from ..models.Product import Product
-from ..models.Comment import Comment
 from ..view import work_with_DB as DB
 from ..view.serializers.SerializerShop import SerializerShop
 from ..view.serializers.SerializerProduct import SerializerProduct
 from ..view.serializers.SerializerComment import SerializerComment
 
 
-
-
-#it's for checking
+# it's method for checking
 def index(request):
     return HttpResponse("Congratulations! It is index page!")
 
 
+# GET
 def select_all(request):
     return JsonResponse(SerializerShop(DB.select_all_Shop(), many=True).data, safe=False)
 
 
-def select_shop_town(request, town):
-    shops = ""
-
-
+# GET
 def select_product_from_shop(request, shopId):
     return JsonResponse(SerializerProduct(DB.select_products_from_shop(shopId), many=True).data, safe=False)
 
 
+# GET
 def get_comments_to_shop(request, shopId):
     return JsonResponse(SerializerComment(DB.get_comments_about_shop(shopId), many=True).data, safe=False)
 
 
-def update_comment(request, shopId, newLine):
-    c = ""
+# POST
+def create_comment(request):
+    bodyComment = request.POST.get('bodyComment')
+    rate = request.POST.get('rate')
+    idShop = request.POST.get('idShop')
+
+    return JsonResponse(SerializerComment(DB.create_comment(bodyComment, rate, idShop), many=False).data, safe=False)
 
 
-def delete_comment(request, shopId, line ):
-    c = ""
+# POST
+def update_comment(request):
+    idComment = request.POST.get('idComment')
+    newRate = request.POST.get('newRate')
+    newBodyOfComment = request.POST.get('newComment')
+
+    return JsonResponse(SerializerComment(DB.update_comment(idComment, newRate, newBodyOfComment), many=False).data,
+                        safe=False)
 
 
-# def create_comment(request, shopId, line, rate_user):
-#     comment = Comment(rate=rate_user, commentLine=line, shopFK=shopId)
-#     try:
-#         comment.save()
-#         return HttpResponse("success", status=200)
-#     except():
-#         return HttpResponse("fail", status=500)
 
+# POST
+def delete_comment(request):
+    idComment = request.POST.get('idComment')
+    DB.delete_comment(idComment)
+    return HttpResponse("Success")
 
--- server/database/view/work_with_DB.py
+++ server/database/view/work_with_DB.py
@@ -13,3 +13,24 @@ def select_products_from_shop(shopId):
 
 def get_comments_about_shop(shopId):
     return Comment.objects.filter(shopFK=shopId)
+
+
+def create_comment(commentOfUser, rateUser, idShop):
+    comment = Comment(rate=rateUser, bodyOfLine=commentOfUser, shopFK=idShop)
+    comment.save()
+    return Comment.objects.filter(rate=rateUser, bodyOfLine=commentOfUser, shopFK=idShop)
+
+
+def update_comment(idComment, newRate, newComment):
+    comment = Comment.objects.filter(id=idComment)
+    comment.rate = newRate
+    comment.commentLine = newComment
+    comment.save()
+    return Comment.objects.filter(id=idComment)
+
+
+def delete_comment(idComment):
+    comment = Comment.objects.filter(id=idComment)
+    comment.delete()
+
+
