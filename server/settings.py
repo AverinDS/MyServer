94abef8417ec2118aa72cbe762c91c4a5b52a45a
AverinDS <awerin960@yandex.ru>
2017-10-20 16:57:11 +0400

    fix some problems with VE

--- server/settings.py
+++ server/settings.py
@@ -31,16 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+
 
     #My pages:
-    'database'
+    'database',
 ]
 
 MIDDLEWARE = [
--- server/wsgi.py
+++ server/wsgi.py
@@ -7,10 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys, site
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+site.addsitedir('/home/dmitry/MyServer/.virtenv/VE/lib/python3.5/site-packages')
+
+sys.path.append('/home/dmitry/MyServer/')
+sys.path.append('/home/dmitry/MyServer/.virtenv/VE/')
+os.environ["DJANGO_SETTINGS_MODULE"] = "server.settings"
+
+
+activate_env=os.path.expanduser("/home/dmitry/MyServer/.virtenv/VE/bin/activate_this.py")
+exec(open(activate_env).read(), dict(__file__=activate_env))
+
+
 
 application = get_wsgi_application()
